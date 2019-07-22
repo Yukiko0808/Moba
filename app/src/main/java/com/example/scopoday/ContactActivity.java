ecdff6a0f50de77eb76c9cdc14ab3d07bae8764a
Hannah <hannahschwegler@gmail.com>
2019-07-22 17:16:58 +0200

    Alter hinzugefuegt

--- app/src/main/java/com/example/scopoday/ContactActivity.java
+++ app/src/main/java/com/example/scopoday/ContactActivity.java
@@ -7,11 +7,14 @@ import android.widget.TextView;
 
 import java.text.Format;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class ContactActivity extends AppCompatActivity {
 
     TextView contactNameText;
     TextView contactBirthday;
+    TextView contactAge;
     SimpleDateFormat sdf = new SimpleDateFormat("dd_mm_yyyy");
 
     @Override
@@ -25,9 +28,21 @@ public class ContactActivity extends AppCompatActivity {
         contactBirthday = findViewById(R.id.ContactBirthday_TV_ID);
         contactBirthday.setText(sdf.format(MainActivity.transmittedContact.getBirthdate()));
 
+        contactAge = findViewById(R.id.ContactAlter_TV_ID);
+        String ageString = Integer.toString(CalculateAge());
+        contactAge.setText(ageString);
+
+
+
      }
 
      private int CalculateAge(){
-        return 1;
+
+        Date today = Calendar.getInstance().getTime();
+
+        int age =  today.getYear() - MainActivity.transmittedContact.getBirthdate().getYear();
+
+
+        return age;
      }
 }
