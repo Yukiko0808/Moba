1d93972d361f246ecd6326c8d77f1d49d5148f7d
Yukiko0808 <hannahschwegler@gmail.com>
2020-01-13 23:06:04 +0100

    Merge branch 'master' of https://github.com/Yukiko0808/Moba
    # Conflicts:
    #	app/src/main/java/com/example/scopoday/ContactActivity.java
    #	app/src/main/java/com/example/scopoday/Contactdata.java

---
+++ app/src/main/java/com/example/scopoday/ZodiacsignActivity.java
@@ -0,0 +1,14 @@
+package com.example.scopoday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ZodiacsignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zodiacsign);
+    }
+}
--- app/src/main/java/com/example/scopoday/ZodiacsignsActivity.java
+++ app/src/main/java/com/example/scopoday/ZodiacsignsActivity.java
@@ -2,13 +2,30 @@ package com.example.scopoday;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ZodiacsignsActivity extends AppCompatActivity {
+public class ZodiacsignsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button aquarius;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.zodiacsigns);
+
+
+        aquarius = (Button) findViewById(R.id.aquarius);
+        aquarius.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent (this, ZodiacsignActivity.class);
+        startActivity(intent);
     }
 }
+
